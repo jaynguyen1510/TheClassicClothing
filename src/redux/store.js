7d568b4faca5e42a5fdc8aa980a466cf8b24a73d
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-06-14 12:05:29 +0700

    cấu hình router và tạo header bọc quanh routes

---
+++ src/redux/store.js
@@ -0,0 +1,9 @@
+// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./slice/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});

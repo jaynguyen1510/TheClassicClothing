fbfb4d9bd7d6948193bef0f08624f36c16cd0fca
jaynguyen1510 <ngnhatnam1510@gmail.com>
2024-09-16 00:37:20 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "~/App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import GlobalStyle from "~/components/GlobalStyle";
 import { Provider } from "react-redux";
